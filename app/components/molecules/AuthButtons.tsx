ecce5e6b70cd101fb6afd655a10d2eb463d5c118
Zulhilmi DN <zulhilmidharma@gmail.com>
2025-11-22 20:32:43 +0700

    add: color schema secondary

--- app/components/molecules/AuthButtons.tsx
+++ app/components/molecules/AuthButtons.tsx
@@ -3,7 +3,7 @@ import { Button } from "../atoms/Button";
 const AuthButtons = () => {
   return (
     <div className="hidden lg:flex lg:items-center lg:gap-2.5">
-      <Button className="button button-secondary">Register</Button>
+      <Button className="button button-light">Register</Button>
       <Button className="button button-primary">Sign In</Button>
     </div>
   );
